ca29165c139b349756878e41f855577cf754ceff
I-linmeng-I <48974182+I-linmeng-I@users.noreply.github.com>
2025-07-05 10:06:10 +0800

    Update libduel.cpp

--- libduel.cpp
+++ libduel.cpp
@@ -237,7 +237,7 @@ int32_t scriptlib::duel_exile(lua_State *L) {
 		pduel->game_field->send_to(pcard, pduel->game_field->core.reason_effect, reason, pduel->game_field->core.reason_player, PLAYER_NONE, 0, 0, POS_FACEUP);
 	else
 		pduel->game_field->send_to(pgroup->container, pduel->game_field->core.reason_effect, reason, pduel->game_field->core.reason_player, PLAYER_NONE, 0, 0, POS_FACEUP);
-	return lua_yieldk(L, 0, (lua_KContext)pduel, [](lua_State *L, int32 status, lua_KContext ctx) {
+	return lua_yieldk(L, 0, (lua_KContext)pduel, [](lua_State *L, int32_t status, lua_KContext ctx) {
 		duel* pduel = (duel*)ctx;
 		lua_pushinteger(L, pduel->game_field->returns.ivalue[0]);
 		return 1;
